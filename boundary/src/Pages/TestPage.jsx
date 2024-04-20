466115ea605190867a53de42b2066ac86e51f3be
jovanjoto <jovan_n@mail.com>
2024-04-20 22:22:15 +0800

    setup color, routes

---
+++ boundary/src/Pages/TestPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function TestPage({}) { 
+  return (
+    <h1>
+      TEST PAGE
+      <h2>
+        This is a test page.
+      </h2>
+    </h1>
+  )
+}
+
+export default TestPage;
--- boundary/src/index.js
+++ boundary/src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import TestPage from "./Pages/TestPage";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/test" element={<TestPage/>}/>
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
