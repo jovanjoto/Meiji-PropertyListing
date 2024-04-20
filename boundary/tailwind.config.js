c429d94164089b1426ee592832c298ec72ae7917
jovanjoto <jovan_n@mail.com>
2024-04-21 00:49:05 +0800

    new vite build

--- boundary/tailwind.config.js
+++ boundary/tailwind.config.js
@@ -1,18 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors : {
-        'purple1' : '#392F5A',
-        'purple2' : '#8B5FBF',
-        'purple3' : '#C989EB',
-        'purple4' : '#F7AEF8',
-        'purple5' : '#FDC5F5'
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
+
