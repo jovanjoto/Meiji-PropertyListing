e5ec7f46e19063d81fbff91b354b8e2b66eabb97
tharvus <atharva.bhavsar1@gmail.com>
2024-04-21 13:55:58 +0800

    updated folder structure

---
+++ boundary/PageComponents/displayList.jsx
@@ -0,0 +1,19 @@
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+} from '@chakra-ui/react'
+  
+import { Input } from '@chakra-ui/react'
+
+export default function displayList({}) {
+    return (
+        <>
+            <Input/>
+        </>
+    )
+}
