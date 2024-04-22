e8e0a5a46b046cd4c15c1a2f2c8266b3de2af317
edbertekaputera <edbertekaputera@gmail.com>
2024-04-23 02:02:06 +0800

    fix web

--- boundary/src/main.jsx
+++ boundary/src/main.jsx
@@ -1,7 +1,6 @@
 // importing libraries
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
