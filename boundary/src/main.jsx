a78a234215b9cb4280ed75a6c2dc2484c4ca57a2
tharvus <atharva.bhavsar1@gmail.com>
2024-04-23 17:16:20 +0800

    resolved merge conflicts(2)

--- boundary/src/main.jsx
+++ boundary/src/main.jsx
@@ -1,31 +1,54 @@
 // importing libraries
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // importing local dependencies
-import LoginPage from './Pages/LoginPage.jsx'
-import RegisterPage from './Pages/RegisterPage.jsx'
-import TestPage from './Pages/TestPage.jsx'
-import ModalTestPage from './Pages/ModalTestPage.jsx'
-import UserAccountManagementPage from './Pages/UserAccountManagementPage.jsx'
-import ProfileManagementPage from './Pages/ProfileManagementPage.jsx'
+import LoginPage from "./Pages/LoginPage.jsx";
+import RegisterPage from "./Pages/RegisterPage.jsx";
+import TestPage from "./Pages/TestPage.jsx";
+import ModalTestPage from "./Pages/ModalTestPage.jsx";
+import UserAccountManagementPage from "./Pages/UserAccountManagementPage.jsx";
+import ProfileManagementPage from "./Pages/ProfileManagementPage.jsx";
+import AuthProvider from "./Components/Authentication/AuthContext.jsx";
+import UnauthenticatedRoute from "./Components/Authentication/UnauthenticatedRoute.jsx";
+import PrivateRoute from "./Components/Authentication/PrivateRoute.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  
-  <BrowserRouter> 
-  <Routes>
-    <Route path='/app' element={<App/>}/>
-    <Route path='/login' element={<LoginPage/>}/>
-    <Route path='/register' element={<RegisterPage/>}/>
-    <Route path='/viewAccounts' element={<UserAccountManagementPage/>}/>
-    <Route path='/viewProfiles' element={<ProfileManagementPage/>}/>
-    <Route path='/test' element={<TestPage/>}/>
-    <Route path='/modalTest' element={<ModalTestPage />} />
-  </Routes>
+ReactDOM.createRoot(document.getElementById("root")).render(
+ <AuthProvider>
+  <BrowserRouter>
+   <Routes>
+    {/* Unauthenticated Routes */}
+    <Route
+     path="/login"
+     element={
+      <UnauthenticatedRoute>
+       <LoginPage />
+      </UnauthenticatedRoute>
+     }
+    />
+    {/* Admin Routes */}
+    <Route
+     path="/admin/viewAccounts"
+     element={
+      <PrivateRoute>
+       <UserAccountManagementPage />
+      </PrivateRoute>
+     }
+    />
+    <Route
+     path="/admin/viewProfiles"
+     element={
+      <PrivateRoute>
+       <ProfileManagementPage />
+      </PrivateRoute>
+     }
+        />
+  {/* Others */}
+  <Route path="/test" element={<TestPage />} />
+    <Route path="/modalTest" element={<ModalTestPage />} />
+   </Routes>
   </BrowserRouter>
-  
-)
+ </AuthProvider>
+);
