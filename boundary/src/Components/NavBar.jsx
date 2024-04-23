5f71710a532cae3df37a8647d7ce9402ac7ba3c1
edbertekaputera <edbertekaputera@gmail.com>
2024-04-24 02:06:53 +0800

    fixed logout

--- boundary/src/Components/NavBar.jsx
+++ boundary/src/Components/NavBar.jsx
@@ -5,6 +5,7 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "./Authentication/AuthContext";
 import { jwtDecode } from "jwt-decode";
+import ConfirmationModal from "./ConfirmationModal";
 
 export default function NavBar({}) {
 	const { logout, token } = useContext(AuthContext);
@@ -14,6 +15,8 @@ export default function NavBar({}) {
 		buying: false,
 		selling: false,
 	});
+	const [confirmationModal, setConfirmationModal] = useState(false);
+
 	const navigate = useNavigate();
 	const { pathname } = useLocation();
 
@@ -39,59 +42,72 @@ export default function NavBar({}) {
 		selling: {},
 	};
 
+	const handleLogout = () => {
+		logout();
+		navigate("/login");
+	};
+
 	return (
-		<Navbar fluid rounded className=" max-w-full bg-gray-100 py-4">
-			<Navbar.Toggle />
-			<Navbar.Collapse>
-				{Object.entries(navigation_links).map(([perm, routes]) => {
-					if (permission[perm]) {
-						return Object.entries(routes).map(([key, value]) => {
-							if (value === pathname) {
+		<>
+			<ConfirmationModal
+				state={confirmationModal}
+				setState={setConfirmationModal}
+				action={handleLogout}
+			>
+				Are you sure you want to logout?
+			</ConfirmationModal>
+			<Navbar fluid rounded className=" max-w-full bg-gray-100 py-4">
+				<Navbar.Toggle />
+				<Navbar.Collapse>
+					{Object.entries(navigation_links).map(([perm, routes]) => {
+						if (permission[perm]) {
+							return Object.entries(routes).map(([key, value]) => {
+								if (value === pathname) {
+									return (
+										<Navbar.Link
+											href={value}
+											active
+											className="text-lg mx-4"
+											key={key}
+										>
+											{key}
+										</Navbar.Link>
+									);
+								}
 								return (
 									<Navbar.Link
 										href={value}
-										active
 										className="text-lg mx-4"
 										key={key}
 									>
 										{key}
 									</Navbar.Link>
 								);
-							}
-							return (
-								<Navbar.Link
-									href={value}
-									className="text-lg mx-4"
-									key={key}
-								>
-									{key}
-								</Navbar.Link>
-							);
-						});
-					}
-				})}
-			</Navbar.Collapse>
+							});
+						}
+					})}
+				</Navbar.Collapse>
 
-			<Dropdown
-				arrowIcon={false}
-				inline
-				label={
-					// <Avatar img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded />
-					<IconContext.Provider value={{ size: "2em" }}>
-						<BsFillPersonFill className="hover:text-gray-500 align-middle mr-5" />
-					</IconContext.Provider>
-				}
-			>
-				<Dropdown.Item
-					onClick={() => {
-						logout();
-						navigate("/login");
-					}}
+				<Dropdown
+					arrowIcon={false}
+					inline
+					label={
+						// <Avatar img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded />
+						<IconContext.Provider value={{ size: "2em" }}>
+							<BsFillPersonFill className="hover:text-gray-500 align-middle mr-5" />
+						</IconContext.Provider>
+					}
 				>
-					Log Out
-				</Dropdown.Item>
-				{/* <Dropdown.Item>View User Account</Dropdown.Item> */}
-			</Dropdown>
-		</Navbar>
+					<Dropdown.Item
+						onClick={() => {
+							setConfirmationModal(true);
+						}}
+					>
+						Log Out
+					</Dropdown.Item>
+					{/* <Dropdown.Item>View User Account</Dropdown.Item> */}
+				</Dropdown>
+			</Navbar>
+		</>
 	);
 }
