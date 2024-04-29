7a48c985ab8997a6b3bb9fdb10f2845a056e93ff
edkesuma <edrickkesuma21@gmail.com>
2024-04-29 17:39:35 +0800

    made create and update controllers to accept form data

--- controller/app/controller/property_listing/update_property_listing.py
+++ controller/app/controller/property_listing/update_property_listing.py
@@ -2,24 +2,62 @@
 from flask import Blueprint, request
 from flask_jwt_extended import jwt_required
 from datetime import datetime, date
+from werkzeug.utils import secure_filename
+import os
 
 # Local dependencies
 from app.entity.propertylisting import PropertyListing
 from app.controller.authentication import permissions_required
+from flask import current_app
 
 class UpdatePropertyListingController(Blueprint):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.add_url_rule("/update_property_listing", view_func=self.updatePL, methods=["PATCH"])
+
+    def allowed_file(self, filename:str):
+        ALLOWED_EXTENSIONS = {'heic', 'png', 'jpg', 'jpeg', 'gif'}
+        return '.' in filename and \
+            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
         
     @permissions_required("has_listing_permission")
     @jwt_required()
     def updatePL(self) -> dict[str,bool]:
-        details = request.get_json()
-        if not details.get("transaction_date"):
-            details["transaction_date"] = date.today()
+        listing_date = request.form.get("listing_date")
+        if not listing_date:
+            listing_date = date.today()
         else:
-            details["transaction_date"] = datetime.strptime(details["transaction_date"], "%Y-%m-%d").date()
+            listing_date = datetime.strptime(listing_date, "%Y-%m-%d").date()
+        
+        if "file" not in request.files:
+            return {"success": False}
+        file = request.files["file"]
+        if file.filename == "" or file.filename == None:
+            return {"success": False}
+        
+        if file and self.allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            image_url = os.path.join("..", current_app.config['UPLOAD_FOLDER'], filename)
+            file.save(image_url)
+            
+            propertyListingJson = {
+                "id": request.form.get("id"),
+                "price": request.form.get("price"),
+                "name": request.form.get("name"),
+                "type": request.form.get("type"),
+                "address": request.form.get("address"),
+                "district": request.form.get("district"),
+                "description": request.form.get("description"),
+                "num_of_bedrooms": request.form.get("num_of_bedrooms"),
+                "num_of_bathrooms": request.form.get("num_of_bathrooms"),
+                "area": request.form.get("area"),
+                "image_url": image_url,
+                "listing_date": listing_date,
+                "agent_email": request.form.get("agent_email"),
+                "seller_email": request.form.get("seller_email")
+            }
 
-        successBool = PropertyListing.updatePL(details)
-        return {"success": successBool}
+            successBool = PropertyListing.updatePL(propertyListingJson)
+            return {"success": successBool}
+        else:
+            return {"success": False}
