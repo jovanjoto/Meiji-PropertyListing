21b34a05d623d4206dfff651ba82070727464cf3
jovanjoto <jovan_n@mail.com>
2024-05-01 00:03:55 +0800

    added property page

--- boundary/vite.config.js
+++ boundary/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
         "/api": {
-            target: "http://localhost:5000",
+            target: "http://127.0.0.1:5000",
             changeOrigin: true,
             secure: false,
         },
