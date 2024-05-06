97d0185fd5dc7fe074990fccf457c526cd26b221
jovanjoto <jovan_n@mail.com>
2024-05-07 01:06:27 +0800

    added view ratings and review page

--- boundary/src/Pages/TestPage.jsx
+++ boundary/src/Pages/TestPage.jsx
@@ -5,23 +5,7 @@ function TestPage({}) {
 	const [show, setShow] = useState(false);
 	return (
 		<h1>
-			<UpdatePropertyModal 
-			state={show} 
-			setState={setShow} 
-			id="3438a820-d44e-4290-850c-0d515aff7c67" 
-			name={"test"}
-			address={"test"}
-			price={1000.0}
-			num_of_bedrooms={1}
-			num_of_bathrooms={1}
-			district={"D01 Boat Quay / Raffles Place / Marina"}
-			property_type={"LANDED"}
-			area={1.0}
-			description={"123"}
-			/>
-			<button type="button" onClick={() => setShow(true)}>
-				Test
-			</button>
+			Test Page
 		</h1>
 	);
 }
--- boundary/src/Pages/ViewPropertyListingPage.jsx
+++ boundary/src/Pages/ViewPropertyListingPage.jsx
@@ -11,77 +11,77 @@ import { AuthContext } from "../Components/Authentication/AuthContext";
 import { jwtDecode } from "jwt-decode";
 
 export default function ViewPropertyListingPage() {
-	const { token } = useContext(AuthContext);
-	const params = useParams();
-	const id = params.id;
-	const [isLoading, setIsLoading] = useState(false);
-	const [property, setProperty] = useState();
-	const navigate = useNavigate();
-	const user = jwtDecode(token);
-	console.log(user.email);
-	useEffect(() => {
-		setIsLoading(true);
-		axios
-			.get(`/api/property_listing/view_property_listing?id=${id}`, {
-				headers: {
-					Authorization: `Bearer ${token}`,
-				},
-			})
-			.then((res) => {
-				if (res.data.success) {
-					setProperty(() => ({
-						...res.data.data,
-						editable: user.email === res.data.data.agent.email,
-					}));
-				} else {
-					navigate("/");
-				}
-			})
-			.catch((error) => {
-				navigate("/");
-			})
-			.then(() => setIsLoading(false));
-	}, [id]);
-	console.log(property);
-	const displayLoading = () => {
-		return (
-			<div className="text-center text-8xl">
-				<Spinner aria-label="Extra large spinner example" size="xl" />
-			</div>
-		);
-	};
+  const { token } = useContext(AuthContext);
+  const params = useParams();
+  const id = params.id;
+  const [isLoading, setIsLoading] = useState(false);
+  const [property, setProperty] = useState();
+  const navigate = useNavigate();
+  const user = jwtDecode(token);
+  console.log(user.email);
+  useEffect(() => {
+    setIsLoading(true);
+    axios
+      .get(`/api/property_listing/view_property_listing?id=${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        if (res.data.success) {
+          setProperty(() => ({
+            ...res.data.data,
+            editable: user.email === res.data.data.agent.email,
+          }));
+        } else {
+          navigate("/");
+        }
+      })
+      .catch((error) => {
+        navigate("/");
+      })
+      .then(() => setIsLoading(false));
+  }, [id]);
+  console.log(property);
+  const displayLoading = () => {
+    return (
+      <div className="text-center text-8xl">
+        <Spinner aria-label="Extra large spinner example" size="xl" />
+      </div>
+    );
+  };
 
-	const displayPL = (data) => {
-		return (
-			<ViewPropertyListingCard
-				id={id}
-				price={data.price}
-				name={data.name}
-				type={data.type}
-				address={data.address}
-				district={data.district}
-				description={data.description}
-				num_of_bedrooms={data.num_of_bedrooms}
-				num_of_bathrooms={data.num_of_bathrooms}
-				area={data.area}
-				image_url={data.image_url}
-				listing_date={data.listing_date}
-				is_sold={data.is_sold}
-				transaction_date={data.transaction_date}
-				seller_email={data.seller_email}
-				agent={data.agent}
-				editable={data.editable}
-			/>
-		);
-	};
+  const displayPL = (data) => {
+    return (
+      <ViewPropertyListingCard
+        id={id}
+        price={data.price}
+        name={data.name}
+        type={data.type}
+        address={data.address}
+        district={data.district}
+        description={data.description}
+        num_of_bedrooms={data.num_of_bedrooms}
+        num_of_bathrooms={data.num_of_bathrooms}
+        area={data.area}
+        image_url={data.image_url}
+        listing_date={data.listing_date}
+        is_sold={data.is_sold}
+        transaction_date={data.transaction_date}
+        seller_email={data.seller_email}
+        agent={data.agent}
+        editable={data.editable}
+      />
+    );
+  };
 
-	if (isLoading || !property) {
-		return displayLoading();
-	}
+  if (isLoading || !property) {
+    return displayLoading();
+  }
 
-	return (
-		<div className="my-24 justify-center items-center align-middle">
-			{displayPL(property)}
-		</div>
-	);
+  return (
+    <div className="my-24 justify-center items-center align-middle">
+      {displayPL(property)}
+    </div>
+  );
 }
