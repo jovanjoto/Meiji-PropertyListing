3a8ca7c67d7507740fb613737582836cca57907d
edbertekaputera <edbertekaputera@gmail.com>
2024-05-06 13:16:02 +0800

    fix

--- controller/app/entity/views.py
+++ controller/app/entity/views.py
@@ -1,43 +1,44 @@
 # Libraries
 from flask import current_app
 from typing_extensions import Self # type: ignore
+from datetime import date
 
 # Local dependencies
 from .sqlalchemy import db
 
 # Views Schema
 class Views(db.Model):
-    __tablename__ = "views"
-    # attributes
-    month = db.Column(db.Integer(), nullable=False, primary_key=True)
-    year = db.Column(db.Integer(), nullable=False, primary_key=True)
-    views = db.Column(db.Integer(), nullable=False, primary_key=True)
+	__tablename__ = "views"
+	# attributes
+	month = db.Column(db.Integer(), nullable=False, primary_key=True)
+	year = db.Column(db.Integer(), nullable=False, primary_key=True)
+	views = db.Column(db.Integer(), nullable=False, primary_key=True)
 
-    # Part of composite key (qualifier)
-    propertyListingId = db.Column(db.String(250), db.ForeignKey("PropertyListing.id"), nullable=False, primary_key=True)
-    viewsToPropertyListingRel = db.relationship("PropertyListing", back_populates="viewsToPropertyListingRel", cascade="all, delete, save-update",
-                                   foreign_keys="Views.id")
-    
-    @classmethod
-    def findPropertyViews(cls, propertyId:str) -> list[Self]:
-        """
-        Queries all Views for a specified property, takes in arguments:
-            - propertyId:str, 
-        returns an list of Views instance.
-        """
-        return cls.query.filter_by(propertyListingId=propertyId).all()
-    
-    @classmethod
-    def incrementViews(cls, propertyId:str, month:int, year:int) -> bool:
-        views = Views.query.filter_by(propertyListingId=propertyId, month=month, year=year).one_or_none()
-        # if views for a property at a certain time doesn't exist, create new views record
-        if not views:
-            newViews = cls(propertyListingId=propertyId, month=month, year=year, views=1)
-            with current_app.app_context():
-                db.session.add(newViews)
-                db.session.commit()
-        # else increment views by 1
-        else:
-            views.views = views.views + 1
+	# Part of composite key (qualifier)
+	propertyListingId = db.Column(db.String(250), db.ForeignKey("PropertyListing.id"), nullable=False, primary_key=True)
+	viewsToPropertyListingRel = db.relationship("PropertyListing", back_populates="viewsToPropertyListingRel", cascade="all, delete, save-update",
+									foreign_keys="Views.id")
 
-        return True
+	@classmethod
+	def findPropertyViews(cls, propertyId:str) -> list[Self]:
+		"""
+		Queries all Views for a specified property, takes in arguments:
+			- propertyId:str, 
+		returns an list of Views instance.
+		"""
+		return cls.query.filter_by(propertyListingId=propertyId).all()
+
+	@classmethod
+	def incrementViews(cls, id:str) -> bool:
+		today = date.today()
+		views = Views.query.filter_by(propertyListingId=id, month=today.month, year=today.year).one_or_none()
+		# if views for a property at a certain time doesn't exist, create new views record
+		with current_app.app_context():
+			if not views:
+				newViews = cls(propertyListingId=id, month=today.month, year=today.year, views=1)
+				db.session.add(newViews)
+			# else increment views by 1
+			else:
+				views.views = views.views + 1
+			db.session.commit()
+		return True
