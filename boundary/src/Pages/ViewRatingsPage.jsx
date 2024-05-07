b97e2d3cd3ccc48c0facc416648abcbf11ce1a39
jovanjoto <jovan_n@mail.com>
2024-05-07 21:50:38 +0800

    connected ratings and reviews

--- boundary/src/Pages/ViewRatingsPage.jsx
+++ boundary/src/Pages/ViewRatingsPage.jsx
@@ -1,49 +1,35 @@
 import StarRatings from "react-star-ratings";
 import CustomerRatingCard from "../Components/Agent/CustomerRatingCard";
 
-function ViewRatingsPage({}) {
-  const ratings = [
-    {
-      firstName: "John",
-      lastName: "Doe",
-      email: "johndoe1@mail.com",
-      phone_num: "123456789",
-      rate: 4.5,
-    },
-    {
-      firstName: "Jane",
-      lastName: "Doe",
-      email: "janedoe2@mail.com",
-      phone_num: "123456790",
-      rate: 5.0,
-    },
-    {
-      firstName: "Bob",
-      lastName: "Smith",
-      email: "bobsmith3@mail.com",
-      phone_num: "123456791",
-      rate: 4.0,
-    },
-    {
-      firstName: "Alice",
-      lastName: "Johnson",
-      email: "alicejohnson4@mail.com",
-      phone_num: "123456792",
-      rate: 4.7,
-    },
-    {
-      firstName: "Charlie",
-      lastName: "Brown",
-      email: "charliebrown5@mail.com",
-      phone_num: "123456793",
-      rate: 3.8,
-    },
-  ];
+import { useState, useEffect, useContext } from 'react';
+import { AuthContext } from '../Components/Authentication/AuthContext';
+import axios from 'axios';
+
+ function ViewRatingsPage({}) {
+
+  const { token } = useContext(AuthContext); 
+
+  const [customerRatings, setCustomerRatings] = useState([]);
+  useEffect(() => {
+		axios
+			.get("/api/rating/view_ratings", {
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+			})
+			.then((response) => {
+        setCustomerRatings(response.data.ratings);
+			})
+			.catch((error) => {
+				console.error("error : ", error);
+			});
+	}, []);
+
 
   return (
     <>
       <div className="flex flex-col gap-5">
-        {ratings.map((rating) => (
+        {customerRatings.map((rating) => (
           <CustomerRatingCard
             firstName={rating.firstName}
             lastName={rating.lastName}
