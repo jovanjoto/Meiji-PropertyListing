75268af15fea406cb7848882b651d4c421ac23f7
jovanjoto <jovan_n@mail.com>
2024-05-13 00:08:03 +0800

    added mortgage calculator modal

--- boundary/src/Pages/TestPage.jsx
+++ boundary/src/Pages/TestPage.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import NumShortlistModal from "../Components/Seller/NumShortlistModal";
 import { Button } from "flowbite-react";
+import MortgageCalculatorModal from "../Components/Buyer/MortgageCalculatorModal";
 
 function TestPage({}) {
 	const [open, setOpen] = useState(false);
 	return (
 		<>
 			<Button onClick={() => {setOpen(true)}}>Toggle modal</Button>
-			<NumShortlistModal setState={setOpen} state={open} propertyName="Alessandrea" propertyId="236bcc1b-2695-4bcd-bb42-0e5fffc93457" />
+			<MortgageCalculatorModal state={open} setState={setOpen}/>
 		</>
 	);
 }
